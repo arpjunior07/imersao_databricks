7b6c93fadd5ac0a52b4358935d24ac56e70080ff
arpjunior07 <arpjunior07>
2025-10-14 22:24:19 +0000

    ajuste

--- pipeline_bronze/transformations/bitcoin_bronze.sql
+++
@@ -1,8 +0,0 @@
-CREATE OR REFRESH STREAMING LIVE TABLE bitcoin
-TBLPROPERTIES ("quality" = "bronze")
-AS
----Le os aquivos JSON usando cloud_files (Auto Loader)
-selecy * from cloud_files(
-  '/Volumes/lakehouse/raw_public/coinbase/coinbase/bitcoin_spot/)',--caminho de origem
-  'json',
-  map ()
